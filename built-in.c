a02c6cda11ebb424a478b0c2381450d7ebe7a55a
KennyReyesS <kennyreyes2000@gmail.com>
2021-04-14 17:43:51 +0000

    comentarios arreglados

--- built-in.c
+++ built-in.c
@@ -12,7 +12,7 @@ int _printenv(char **environ)
 
 	while (environ[i] != NULL)
 	{
-		length = _strlen(environ[i]); /* almacena la longitud del la lista del env */
+		length = _strlen(environ[i]); /* almacena la longitud del env */
 		write(STDOUT_FILENO, environ[i], length); /* y lo imprime con la funcion write */
 		write(STDOUT_FILENO, "\n", 1); /* luego salta la linea */
 		i++;
@@ -24,7 +24,7 @@ int _printenv(char **environ)
  *_getenv - get the env if a user type other command (no "exit", no "env")
  *@name: pointer to the name of the environment variable
  *@env: pointer char to the string of the enviroment list.
- *Return: the string to the command in the path or NULL if "Not found".
+ *Return: The direction of the directories of env variable or "Not found".
  */
 
 char *_getenv(const char *name, char **env)
@@ -36,17 +36,17 @@ char *_getenv(const char *name, char **env)
 		return (NULL);
 	while (env[i] != NULL) /* mientras el env no sea nulo */
 	{
-		while (env[i][j] == name[j]) /* si encuentra el nombre de la variable de entorno */
+		while (env[i][j] == name[j]) /* si encuentra el nombre de la variable de entorno por ejemplo la variable PATH */
 			j++; /* revisara la linea donde se encuentra la variable de entorno, por ejemplo PATH=/usr/local/sbin:/usr/local/bin: */
 		if (env[i][j] == '=') /* si encuentra un = */
 		{
 			j++; /* avanza j */
 			return (&(env[i][j])); /* retorna la direccion de la variable de entorno despues del = por ejemplo en el caso del PATH:
-						  /usr/local/sbin:/usr/local/bin: */
+						  solo retornara: /usr/local/sbin:/usr/local/bin: */
 		}
 		i++;
 		j = 0;
 	}
-	write(STDOUT_FILENO, "Not found", 9); /* si no encuentra imprimira Not found */
+	write(STDOUT_FILENO, "Not found", 9); /* si no encuentra la variable de entorno imprimira Not found */
 	return (NULL);
 }
